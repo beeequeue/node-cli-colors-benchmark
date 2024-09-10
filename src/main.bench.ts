3e2cf8c4bff7b7a24dd3d9cd0d3af8bff738101e
Adam <adam@haglund.dev>
2024-09-10 20:36:21 +0200

    fix bench

--- src/main.bench.ts
+++ src/main.bench.ts
@@ -5,36 +5,64 @@ import ansis from "ansis"
 import chalk from "chalk"
 import * as colorette from "colorette"
 import picocolors from "picocolors"
-import { bench, describe } from "vitest"
+import { type BenchOptions, bench, describe } from "vitest"
 
 import { pkgs } from "./packages"
+
+const options = {
+  time: 100,
+} satisfies BenchOptions
 
 describe(
   "one (red)",
   () => {
-    bench(pkgs.native, () => {
-      styleText("red", "foo")
-    })
+    bench(
+      pkgs.native,
+      () => {
+        styleText("red", "foo")
+      },
+      options,
+    )
 
-    bench(pkgs["ansi-colors"], () => {
-      ansiColors.red("foo")
-    })
+    bench(
+      pkgs["ansi-colors"],
+      () => {
+        ansiColors.red("foo")
+      },
+      options,
+    )
 
-    bench(pkgs.ansis, () => {
-      ansis.red("foo")
-    })
+    bench(
+      pkgs.ansis,
+      () => {
+        ansis.red("foo")
+      },
+      options,
+    )
 
-    bench(pkgs.chalk, () => {
-      chalk.red("foo")
-    })
+    bench(
+      pkgs.chalk,
+      () => {
+        chalk.red("foo")
+      },
+      options,
+    )
 
-    bench(pkgs.colorette, () => {
-      colorette.red("foo")
-    })
+    bench(
+      pkgs.colorette,
+      () => {
+        colorette.red("foo")
+      },
+      options,
+    )
 
-    bench(pkgs.picocolors, () => {
-      picocolors.red("foo")
-    })
+    bench(
+      pkgs.picocolors,
+      () => {
+        picocolors.red("foo")
+      },
+      options,
+    )
   },
   { sequential: true },
 )
@@ -42,116 +70,194 @@ describe(
 describe(
   "two (red, bold)",
   () => {
-    bench(pkgs.native, () => {
-      styleText("red", styleText("bold", "foo"))
-    })
+    bench(
+      pkgs.native,
+      () => {
+        styleText("red", styleText("bold", "foo"))
+      },
+      options,
+    )
 
-    bench(pkgs["ansi-colors"], () => {
-      ansiColors.red.bold("foo")
-    })
+    bench(
+      pkgs["ansi-colors"],
+      () => {
+        ansiColors.red.bold("foo")
+      },
+      options,
+    )
 
-    bench(pkgs.ansis, () => {
-      ansis.red.bold("foo")
-    })
+    bench(
+      pkgs.ansis,
+      () => {
+        ansis.red.bold("foo")
+      },
+      options,
+    )
 
-    bench(pkgs.chalk, () => {
-      chalk.red.bold("foo")
-    })
+    bench(
+      pkgs.chalk,
+      () => {
+        chalk.red.bold("foo")
+      },
+      options,
+    )
 
-    bench(pkgs.colorette, () => {
-      colorette.red(colorette.bold("foo"))
-    })
+    bench(
+      pkgs.colorette,
+      () => {
+        colorette.red(colorette.bold("foo"))
+      },
+      options,
+    )
 
-    bench(pkgs.picocolors, () => {
-      picocolors.red(picocolors.bold("foo"))
-    })
+    bench(
+      pkgs.picocolors,
+      () => {
+        picocolors.red(picocolors.bold("foo"))
+      },
+      options,
+    )
   },
-  { sequential: true },
+  { sequential: true, repeats: 20_000 },
 )
 
 describe(
   "three (blue, bold, italic)",
   () => {
-    bench(pkgs.native, () => {
-      styleText("blue", styleText("bold", styleText("italic", "foo")))
-    })
+    bench(
+      pkgs.native,
+      () => {
+        styleText("blue", styleText("bold", styleText("italic", "foo")))
+      },
+      options,
+    )
 
-    bench(pkgs["ansi-colors"], () => {
-      ansiColors.blue.bold.italic("foo")
-    })
+    bench(
+      pkgs["ansi-colors"],
+      () => {
+        ansiColors.blue.bold.italic("foo")
+      },
+      options,
+    )
 
-    bench(pkgs.ansis, () => {
-      ansis.blue.bold.italic("foo")
-    })
+    bench(
+      pkgs.ansis,
+      () => {
+        ansis.blue.bold.italic("foo")
+      },
+      options,
+    )
 
-    bench(pkgs.chalk, () => {
-      chalk.blue.bold.italic("foo")
-    })
+    bench(
+      pkgs.chalk,
+      () => {
+        chalk.blue.bold.italic("foo")
+      },
+      options,
+    )
 
-    bench(pkgs.colorette, () => {
-      colorette.blue(colorette.bold(colorette.italic("foo")))
-    })
+    bench(
+      pkgs.colorette,
+      () => {
+        colorette.blue(colorette.bold(colorette.italic("foo")))
+      },
+      options,
+    )
 
-    bench(pkgs.picocolors, () => {
-      picocolors.blue(picocolors.bold(picocolors.italic("foo")))
-    })
+    bench(
+      pkgs.picocolors,
+      () => {
+        picocolors.blue(picocolors.bold(picocolors.italic("foo")))
+      },
+      options,
+    )
+  },
+  { sequential: true, repeats: 20_000 },
+)
+
+describe(
+  "complex",
+  () => {
+    bench(
+      pkgs.native,
+      () => {
+        styleText(
+          "blue",
+          styleText(
+            "cyan",
+            styleText(
+              "red",
+              styleText(
+                "yellow",
+                styleText(
+                  "magenta",
+                  styleText("bgBlack", styleText("bold", styleText("italic", "foo"))),
+                ),
+              ),
+            ),
+          ),
+        )
+      },
+      options,
+    )
+
+    bench(
+      pkgs["ansi-colors"],
+      () => {
+        ansiColors.bgBlack.magenta.yellow.red.cyan.blue.bold.italic("foo")
+      },
+      options,
+    )
+
+    bench(
+      pkgs.ansis,
+      () => {
+        ansis.bgBlack.magenta.yellow.red.cyan.blue.bold.italic("foo")
+      },
+      options,
+    )
+
+    bench(
+      pkgs.chalk,
+      () => {
+        chalk.bgBlack.magenta.yellow.red.cyan.blue.bold.italic("foo")
+      },
+      options,
+    )
+
+    bench(
+      pkgs.colorette,
+      () => {
+        colorette.bgBlack(
+          colorette.magenta(
+            colorette.yellow(
+              colorette.red(
+                colorette.cyan(colorette.blue(colorette.bold(colorette.italic("foo")))),
+              ),
+            ),
+          ),
+        )
+      },
+      options,
+    )
+
+    bench(
+      pkgs.picocolors,
+      () => {
+        picocolors.bgBlack(
+          picocolors.magenta(
+            picocolors.yellow(
+              picocolors.red(
+                picocolors.cyan(
+                  picocolors.blue(picocolors.bold(picocolors.italic("foo"))),
+                ),
+              ),
+            ),
+          ),
+        )
+      },
+      options,
+    )
   },
   { sequential: true },
 )
-
-describe("complex", () => {
-  bench(pkgs.native, () => {
-    styleText(
-      "blue",
-      styleText(
-        "cyan",
-        styleText(
-          "red",
-          styleText(
-            "yellow",
-            styleText(
-              "magenta",
-              styleText("bgBlack", styleText("bold", styleText("italic", "foo"))),
-            ),
-          ),
-        ),
-      ),
-    )
-  })
-
-  bench(pkgs["ansi-colors"], () => {
-    ansiColors.bgBlack.magenta.yellow.red.cyan.blue.bold.italic("foo")
-  })
-
-  bench(pkgs.ansis, () => {
-    ansis.bgBlack.magenta.yellow.red.cyan.blue.bold.italic("foo")
-  })
-
-  bench(pkgs.chalk, () => {
-    chalk.bgBlack.magenta.yellow.red.cyan.blue.bold.italic("foo")
-  })
-
-  bench(pkgs.colorette, () => {
-    colorette.bgBlack(
-      colorette.magenta(
-        colorette.yellow(
-          colorette.red(
-            colorette.cyan(colorette.blue(colorette.bold(colorette.italic("foo")))),
-          ),
-        ),
-      ),
-    )
-  })
-
-  bench(pkgs.picocolors, () => {
-    picocolors.bgBlack(
-      picocolors.magenta(
-        picocolors.yellow(
-          picocolors.red(
-            picocolors.cyan(picocolors.blue(picocolors.bold(picocolors.italic("foo")))),
-          ),
-        ),
-      ),
-    )
-  })
-})
